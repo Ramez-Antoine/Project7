e25cf123cc8eeac202a58c3673b14c4a7613118d
youssef reda <yreda0232@gmail.com>
2025-11-21 01:35:55 +0200

    .

--- src/project7/InstructorFrame.java
+++ src/project7/InstructorFrame.java
@@ -152,6 +152,8 @@ public class InstructorFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+       MainMenuFrame mainMenu=new MainMenuFrame();
+        mainMenu.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
